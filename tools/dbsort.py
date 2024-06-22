21476fcd653af554aa989687e6a0b9fa38deb73c
Natrium <1583803226@qq.com>
2024-06-22 13:52:48 +0800

    open data_file with utf-8

--- tools/dbsort.py
+++ tools/dbsort.py
@@ -3,10 +3,10 @@ import os
 if __name__ == '__main__':
     rootdir = os.path.dirname(os.path.dirname(__file__))
     data_file = os.path.join(rootdir, 'scripts', 'data.js')
-    with open(data_file, 'r') as f:
+    with open(data_file, 'r', encoding='utf-8') as f:
         data = f.readlines()[2:-1]
         data.sort()
-    with open(data_file, "w") as f:
+    with open(data_file, "w", encoding="utf-8") as f:
         f.write("const nameDB =\n{\n")
         for line in data:
             f.write(line.replace("\'", "\""))
